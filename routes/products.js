2ca510106b4039ed400bff94c4908ce628acd34c
Diego <dhiego1510@gmail.com>
2024-03-07 17:34:01 -0500

    moving endpoints to routes folder

---
+++ routes/products.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { faker } = require('@faker-js/faker')
+
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    const products = [];
+    const { size } = req.query;
+    const limit = size || 10;
+
+    for (let i = 0; i < limit; i++) {
+      products.push({
+        name: faker.commerce.productName(),
+        price: parseInt(faker.commerce.price(), 10),
+        image: faker.image.imageUrl()
+      })
+
+    }
+    res.json(products);
+  })
+
+  router.get('/filter', (req, res) => {
+    res.send('I am a filter')
+  }) // Este es un edpoint especifico
+
+  // --------------Todo lo que sea especifico tiene que ir antes de lo que sesa dinamico---------------//
+  router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({
+      id,
+      name: 'laptop',
+      price: 100
+    })
+  }) // Este es un edpoint dinamico
+
+  router.post('/', (req, res) => {
+    const body = req.body;
+    res.json({
+      message: 'created ',
+      data: body
+    })
+  })
+  module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    });
+  } else {
+    res.send('there are no parameters');
+  }
+})// Esto son parametros tipo query, sus nombres son limit y offset , si existen devolvemos el valor sino decimos que no se encontraron parametros
+
+module.exports = router;
